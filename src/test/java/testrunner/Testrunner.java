193fa3ca92c5f5fd1fa7abf357d6217c860800e8
vamshikrishna-cstech <129050981+vamshikrishna-cstech@users.noreply.github.com>
2024-09-18 21:49:25 +0530

    update

--- src/test/java/testrunner/Testrunner.java
+++ src/test/java/testrunner/Testrunner.java
@@ -1,12 +1,12 @@
 package testrunner;
 
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {"classpath:features"},  // Default path; this will be overridden by the system property if set
+    features = "classpath:features",  // Use classpath for features path
     glue = {"stepdefinition", "pages"},
     tags = "@smoketest or @regression",
     monochrome = true,
