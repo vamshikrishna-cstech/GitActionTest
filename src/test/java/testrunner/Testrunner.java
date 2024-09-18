23017dc2cd97e4591f9ccebd610e593ba6c25384
vamshikrishna-cstech <129050981+vamshikrishna-cstech@users.noreply.github.com>
2024-09-18 17:29:32 +0530

    yaml

--- src/test/java/testrunner/Testrunner.java
+++ src/test/java/testrunner/Testrunner.java
@@ -6,13 +6,18 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
-    glue = {"stepdefinition","pages"},
-    tags = "@smoketest or @regression",
+    features = "@features",  // Placeholder to be replaced by system property
+    glue = {"stepdefinition", "pages"},
+    tags = "@smoketest or @regression", // You can dynamically modify tags if needed
     monochrome = true,
     plugin = {
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
     }
 )
 public class Testrunner {
+    static {
+        // Replace placeholder with actual path from system property
+        String featuresPath = System.getProperty("features", "src/test/resources/features");
+        System.setProperty("features", featuresPath);
+    }
 }
