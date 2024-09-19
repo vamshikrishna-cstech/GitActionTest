ba3dd277bac19c1a36c29c4a77188df39c31f56b
vamshikrishna-cstech <129050981+vamshikrishna-cstech@users.noreply.github.com>
2024-09-19 20:11:34 +0530

    update

--- src/test/java/pages/Login_page.java
+++ src/test/java/pages/Login_page.java
@@ -11,7 +11,7 @@ public class Login_page {
 	@FindBy(css = "input[id='email']")
 	WebElement txt_username;
 
-	@FindBy(css = "input[id='password1']")
+	@FindBy(css = "input[id='password']")
 	WebElement txt_password;
 
 	@FindBy(xpath = "//div[contains(text(),'Login')]")
